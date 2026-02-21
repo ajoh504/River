a74d0a3f4e3a1efc6045441c6cdf37bf1840cd55
ajoh504 <alexander.johnson504@gmail.com>
2026-02-21 08:05:12 -0800

    chore: add FileSystemWatcher functionality

--- River.Infrastructure/FileSystem/FileChangeListener.cs
+++ River.Infrastructure/FileSystem/FileChangeListener.cs
@@ -4,6 +4,60 @@ namespace River.Infrastructure.FileSystem
 {
     public class FileChangeListener : IFileChangeListener
     {
+        private FileSystemWatcher _fileSystemWatcher;
+        public FileChangeListener(FileSystemWatcher fileSystemWatcher)
+        {
+            _fileSystemWatcher = fileSystemWatcher;
+            _fileSystemWatcher.NotifyFilter = NotifyFilters.Attributes
+                | NotifyFilters.CreationTime
+                | NotifyFilters.DirectoryName
+                | NotifyFilters.FileName
+                | NotifyFilters.LastAccess
+                | NotifyFilters.LastWrite
+                | NotifyFilters.Security
+                | NotifyFilters.Size;
 
+            _fileSystemWatcher.Changed += OnChanged;
+            _fileSystemWatcher.Created += OnCreated;
+            _fileSystemWatcher.Deleted += OnDeleted;
+            _fileSystemWatcher.Renamed += OnRenamed;
+            _fileSystemWatcher.Error += OnError;
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Created)
+            {
+                return;
+            }
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Deleted)
+            {
+                return;
+            }
+        }
+
+        private static void OnRenamed(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Renamed)
+            {
+                return;
+            }
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e)
+        {
+        }
     }
 }
