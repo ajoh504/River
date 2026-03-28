1930d0aa77e7d9bac9d7523adf5042a90cdec1e4
ajoh504 <alexander.johnson504@gmail.com>
2026-03-28 11:20:49 -0700

    chore: restructure database access into EF folder/namespace

---
+++ src/River.Infrastructure/DataAccessLayer/EF/Entities/File.cs
@@ -0,0 +1,11 @@
+﻿using River.Infrastructure.DataAccessLayer.EF.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace River.Infrastructure.DataAccessLayer.EF.Entities
+{
+    public class File : IFile
+    {
+    }
+}
--- src/River.Infrastructure/DataAccessLayer/EF/Interfaces/IFile.cs
+++ src/River.Infrastructure/DataAccessLayer/EF/Interfaces/IFile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace River.Infrastructure.DataAccessLayer.Interfaces
+namespace River.Infrastructure.DataAccessLayer.EF.Interfaces
 {
     internal interface IFile
     {
