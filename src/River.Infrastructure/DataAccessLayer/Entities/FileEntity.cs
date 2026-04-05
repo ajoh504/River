0fc855eca2e2af145c354e297df5fa96d3f99503
ajoh504 <alexander.johnson504@gmail.com>
2026-04-05 09:22:15 -0700

    chore: add overloaded constructors to handle save operations and select operations

--- src/River.Infrastructure/DataAccessLayer/Entities/FileEntity.cs
+++ src/River.Infrastructure/DataAccessLayer/Entities/FileEntity.cs
@@ -2,12 +2,31 @@
 {
     public class FileEntity
     {
-        public long Id { get; init; }
+        public long Id { get; private set; }
         public string? Name { get; private set; }
         public string? Path { get; set; }
         public string? Extension { get; private set; }
-        public long DirectoryId { get; init; }
+        public long DirectoryId { get; private set; }
         public bool Ignore { get; set; }
         public bool Inactive { get; set; }
+        public FileEntity(string path, long directoryId, bool ignore = false, bool inactive = false)
+        {
+            Name = System.IO.Path.GetFileNameWithoutExtension(path);
+            Path = path;
+            Extension = System.IO.Path.GetExtension(path);
+            DirectoryId = directoryId;
+            Ignore = ignore;
+            Inactive = inactive;
+        }
+        public FileEntity(long id, string name, string path, string extension, long directoryId, bool ignore = false, bool inactive = false)
+        {
+            Id = id;
+            Name = name;
+            Path = path;
+            Extension = extension;
+            DirectoryId = directoryId;
+            Ignore = ignore;
+            Inactive = inactive;
+        }
     }
 }
