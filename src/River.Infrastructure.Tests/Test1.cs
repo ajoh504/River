9e1bb9af6ba69836e10632835019183198ed91cf
ajoh504 <alexander.johnson504@gmail.com>
2026-04-05 06:07:04 -0700

    test: add initial Infrastructure.Tests project

---
+++ src/River.Infrastructure.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace River.Infrastructure.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
